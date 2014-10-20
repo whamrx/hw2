510f389e482a0f714042298e0f408f8e096fb02e
Nate Foster <jnfoster@cs.cornell.edu>
2014-10-20 02:24:06 -0400

    Q1 and Q3

--- hw2/src/Q1.ml
+++ hw2/src/Q1.ml
@@ -1,7 +1,16 @@
 open NetKAT.Std
 
-let hub : policy = 
-    failwith "Not yet implemented"
+(* a simple repeater *)
+let all_ports : int32 list = [1l; 2l; 3l; 4l]
 
-let _ = run_static hub
+let flood (n : int32) : policy =
+	List.fold_left
+    	(fun pol m -> if n = m then pol else <:netkat<$pol + port := $m>>)
+    	<:netkat<drop>> all_ports
 
+let repeater : policy =
+	List.fold_right
+    	(fun m pol -> let p = flood m in <:netkat<if port = $m then $p else $pol>>)
+    	all_ports <:netkat<drop>>
+
+let _ = run_static repeater
---
+++ hw2/src/Q3.ml
@@ -0,0 +1,10 @@
+open NetKAT.Std
+open Q1
+
+let firewall : policy =
+  	<:netkat<
+    		if port = 1 && ethType = 0x0800 && ipProto = 0x06 && tcpDstPort = 22 then drop 
+		else $repeater
+  	>>
+
+let _ = run_static firewall
